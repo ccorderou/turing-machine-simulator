32b54c093d9226b7dd362f0a20175140b3006575
Chris <chrisatcompsci@gmail.com>
2025-03-08 18:26:09 -0500

    introduce file reading capabilities

--- turingMachine.cpp
+++ turingMachine.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-
+#include <fstream>
 
 int main(){
 
-
+    //Prompt user for file name
+    std::cout << "Enter a file name: ";
+    string fileName{};
+    std::getline(cin, fileName);
+    //Instantiate file-reading object
+    std::ifstream inputFile(fileName);
+     
     return 0;
 }
