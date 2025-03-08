8d45aad97c9ffb07242764bf644008d6ccff97a0
Chris <chrisatcompsci@gmail.com>
2025-03-08 18:47:45 -0500

    declare a temporary struct that organizes the code of the Turing Machine

--- turingMachine.cpp
+++ turingMachine.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
-int main(){
+/**
+    @param: Takes in the current state and current symbol
+    @post: Returns the new state, new type symbol, and direction in a string
+*/
+std::string transitionFunction(std::string currentState, std::string currentTypeSymbol);
 
-    //Prompt user for file name
+struct Components
+{
+    std::vector<std::string> states;
+    std::vector<int> inputSymbols;
+    std::vector<std::string> tapeSymbols;
+    std::string transitionFunction(std::string currentState, std::string currentTypeSymbol);
+    std::string startState = "q1";
+    std::string blankSymbol = "B";
+    std::vector<std::string> acceptingStates;
+};
+
+int main()
+{
+
+    // Prompt user for file name
     std::cout << "Enter a file name: ";
     std::string fileName{};
     std::getline(std::cin, fileName);
-    //Instantiate file-reading object
+    // Instantiate file-reading object
     std::ifstream inputFile(fileName);
 
     std::string fileLine{};
-    while( std::getline(inputFile, fileLine)){
+    while (std::getline(inputFile, fileLine))
+    {
         std::cout << fileLine << std::endl;
     }
     return 0;
