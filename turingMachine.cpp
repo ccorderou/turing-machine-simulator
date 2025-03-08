81abd5c046b1c8fde1a0faa67f60db928d7609c0
Chris <chrisatcompsci@gmail.com>
2025-03-08 18:31:32 -0500

    ensure proper file reading

--- turingMachine.cpp
+++ turingMachine.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(){
 
     //Prompt user for file name
     std::cout << "Enter a file name: ";
-    string fileName{};
-    std::getline(cin, fileName);
+    std::string fileName{};
+    std::getline(std::cin, fileName);
     //Instantiate file-reading object
     std::ifstream inputFile(fileName);
-     
+
+    std::string fileLine{};
+    while( std::getline(inputFile, fileLine)){
+        std::cout << fileLine << std::endl;
+    }
     return 0;
 }
