d612e7a42d326632bcecd1ff31b1011b29b81ced
Chris <chrisatcompsci@gmail.com>
2025-03-08 23:17:50 -0500

    read states from file

--- turingMachine.cpp
+++ turingMachine.cpp
@@ -2,23 +2,30 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <cctype>
 
 /**
     @param: Takes in the current state and current symbol
     @post: Returns the new state, new type symbol, and direction in a string
 */
-std::string transitionFunction(std::string currentState, std::string currentTypeSymbol);
+// std::string transitionFunction(std::string currentState, std::string currentTypeSymbol);
 
-struct Components
+struct TuringMachine
 {
     std::vector<std::string> states;
-    std::vector<int> inputSymbols;
+    std::string inputSymbols[2] = {"0", "1"};
     std::vector<std::string> tapeSymbols;
-    std::string transitionFunction(std::string currentState, std::string currentTypeSymbol);
-    std::string startState = "q1";
+    std::unordered_map<std::string, std::string> transitionFunction;
+    // std::string transitionFunction(std::string currentState, std::string currentTypeSymbol);
+    std::string startState = "q0";
     std::string blankSymbol = "B";
     std::vector<std::string> acceptingStates;
+    std::vector<std::string> tape;
 };
+
+// helper function to reduce redundancies
+bool contains(const std::vector<std::string> container, const std::string &letter);
 
 int main()
 {
@@ -30,10 +37,27 @@ int main()
     // Instantiate file-reading object
     std::ifstream inputFile(fileName);
 
+    // Each line is either:
+    //  1) blank
+    //  2) a comment indicated by "//"
+    //  3) or a 5-tuple consisting of currentState currentTapeSymbol newState newTapeSymbol direction
     std::string fileLine{};
+    TuringMachine simulator;
     while (std::getline(inputFile, fileLine))
     {
-        std::cout << fileLine << std::endl;
+        // if a line begins with an integer, it must be a transition function
+        if (std::isdigit(fileLine[0]))
+        {
+            if (!contains(simulator.states, std::string(1, fileLine[0])))
+                simulator.states.push_back(std::string(1, fileLine[0]));
+        }
     }
-    return 0;
+}
+
+bool contains(const std::vector<std::string> container, const std::string &letter_val)
+{
+    for (const auto &letter : container)
+        if (letter == letter_val)
+            return true;
+    return false;
 }
